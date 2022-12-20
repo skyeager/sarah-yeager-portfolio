37977210c9df12447e2113f3a5c00a45b779f779
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-12-20 14:07:24 -0500

    painted white background

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/speckled-cloud.jpg";
+import image from "../images/painted-white.jpg";
 
-const imageAltText = "Speckled cloud";
+const imageAltText = "Painted white background";
 
 /**
  * Sort description that expands on your title on the Home component.
