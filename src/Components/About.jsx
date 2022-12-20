50d6e07d9832a2dd0a5d6bb37404385a2e44dd35
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-12-20 13:42:07 -0500

    changed about myself to about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -58,7 +58,7 @@ const About = () => {
           textAlign: "center",
         }}
       >
-        <h2>About Myself</h2>
+        <h2>About</h2>
         <p className="large">{description}</p>
         <hr />
         <ul
